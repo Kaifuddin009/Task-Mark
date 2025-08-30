d17f330e64bdcbf548c70f83df398c4a6d72b28a
kaifuddin009 <kaifuddin01@email.com>
2025-08-30 17:14:47 +0530

    .

--- client/src/pages/Homepages.jsx
+++ client/src/pages/Homepages.jsx
@@ -13,7 +13,7 @@ useEffect(() => {
     try {
       const res = await axiosInstance.get("/notes");
       console.log(res.data);
-      setNotes(res.data.notes)
+      setNotes(res.data.notes || [])
     } catch (error) {
       console.log("Error fetching notes",error)
     }finally{
@@ -28,8 +28,8 @@ useEffect(() => {
       <Navbar/>
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
-        {notes.length === 0 && <NotesNotFound/>}
-        {notes.length > 0 && (
+        {Array.isArray(notes) && notes.length === 0 && <NotesNotFound/>}
+        {Array.isArray(notes) && notes.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
               <Notecard key={note.id} note={note} setNotes={setNotes}/>
